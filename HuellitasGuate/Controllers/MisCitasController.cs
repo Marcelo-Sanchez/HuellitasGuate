e7c828b7682a123e4b78a548862548c4f2939f4d
Marcelo Sanchez <wmsanchez11@gmail.com>
2023-09-20 21:36:39 -0600

    Nuevo Modulo de mis citas y preferencias de contraseña

---
+++ HuellitasGuate/Controllers/MisCitasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HuellitasGuate.Data;
+using HuellitasGuate.Models;
+
+namespace HuellitasGuate.Controllers
+{
+    public class MisCitasController : Controller
+    {
+        private readonly HuellitasGuateContext _context;
+
+        public MisCitasController(HuellitasGuateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MisCitas
+        public async Task<IActionResult> Index()
+        {
+              return _context.Citas != null ? 
+                          View(await _context.Citas.ToListAsync()) :
+                          Problem("Entity set 'HuellitasGuateContext.Citas'  is null.");
+        }
+
+        // GET: MisCitas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // GET: MisCitas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MisCitas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Mascota,Servicio,Telefono,Fecha,Dpi,Correo,Descripcion")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cita);
+        }
+
+        // GET: MisCitas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            return View(cita);
+        }
+
+        // POST: MisCitas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Mascota,Servicio,Telefono,Fecha,Dpi,Correo,Descripcion")] Cita cita)
+        {
+            if (id != cita.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitaExists(cita.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cita);
+        }
+
+        // GET: MisCitas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Citas == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // POST: MisCitas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Citas == null)
+            {
+                return Problem("Entity set 'HuellitasGuateContext.Citas'  is null.");
+            }
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita != null)
+            {
+                _context.Citas.Remove(cita);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitaExists(int id)
+        {
+          return (_context.Citas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- HuellitasGuate/Program.cs
+++ HuellitasGuate/Program.cs
@@ -4,53 +4,109 @@ using HuellitasGuate.Data;
 using HuellitasGuate.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-string connectionString = string.Empty;
 
-
-if (builder.Configuration.GetConnectionString("env") == "local")
+public class Program
 {
-    connectionString = builder.Configuration.GetConnectionString("HuellitasGuateContextConnection");
+    public static async Task Main(string[] args)
+    { 
+    
+        var builder = WebApplication.CreateBuilder(args);
+        string connectionString = string.Empty;
+
+
+        if (builder.Configuration.GetConnectionString("env") == "local")
+        {
+            connectionString = builder.Configuration.GetConnectionString("HuellitasGuateContextConnection");
+        }
+
+        else
+        {
+            connectionString = builder.Configuration.GetConnectionString("HuellitasGuateDevContextConnection");
+        }
+
+
+        builder.Services.AddDbContext<HuellitasGuateContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        builder.Services.AddDefaultIdentity<HuellitasGuateUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 3;
+        })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<HuellitasGuateContext>();
+
+
+
+        builder.Services.AddScoped<IEmailSender, EmailSender>();
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();;
+        app.MapRazorPages();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    using(var scope = app.Services.CreateScope())
+    {
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+
+        var roles = new[] { "Admin", "Manager", "Member" };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+                await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var userManager = 
+            scope.ServiceProvider.GetRequiredService<UserManager<HuellitasGuateUser>>();
+
+        string email = "wmsanchez11@gmail.com";
+        string password = "Test1234,";
+
+        if(await userManager.FindByEmailAsync(email) == null)
+        {
+            var user = new HuellitasGuateUser();
+            user.UserName = email;
+            user.Email = email;
+            //user.EmailConfirmed = true;
+
+            await userManager.CreateAsync(user, password);
+
+            await userManager.AddToRoleAsync(user, "Admin");
+
+
+         }
+    }
+
+        app.Run();
+
+
+    }
 }
-
-else
-{
-    connectionString = builder.Configuration.GetConnectionString("HuellitasGuateDevContextConnection");
-}
-
-
-builder.Services.AddDbContext<HuellitasGuateContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<HuellitasGuateUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<HuellitasGuateContext>();
-
-builder.Services.AddScoped<IEmailSender, EmailSender>();
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication();;
-app.MapRazorPages();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-
-
